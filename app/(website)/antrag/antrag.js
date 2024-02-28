756191d921fdb380d24472caaea3a1e17dcaf0eb
Lucas Prabant <lprabant@gmail.com>
2024-02-28 21:35:08 +0100

    antrag drinnen, url passt auch

--- app/(website)/antrag/antrag.js
+++ app/(website)/antrag/antrag.js
@@ -1,8 +1,12 @@
 import Container from '@/components/container'
+
 export default function Antrag() {
   return (
     <Container>
-      {"under construction"}
+      <iframe
+        src='https://docs.google.com/forms/d/e/1FAIpQLSet2cQG6o85yy9aqgwhmxVEuoWCrbj2dMofqIBZp2b-XvrxBQ/viewform?embedded=true'
+        width='100%' height='600' className={'m-auto'} frameBorder='20' marginHeight='0' marginWidth='0'>Loading…
+      </iframe>
     </Container>
   )
 }
