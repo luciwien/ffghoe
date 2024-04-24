b170b336f8ff087dd844faa699379383f071d17f
Lucas Prabant <lprabant@gmail.com>
2024-04-24 16:42:41 +0200

    added some stuff

---
+++ app/(website)/queerfootballheroes/page.js
@@ -0,0 +1,7 @@
+import Queerfootballheroes from "./queerfootballheroes";
+
+export default async function QueerfootballheroesPage() {
+  return <Queerfootballheroes />;
+}
+
+// export const revalidate = 60;
--- lib/sanity/schemas/index.ts
+++ lib/sanity/schemas/index.ts
@@ -10,3 +10,36 @@ import friends from './friends'
 
 export const schemaTypes = [about, post, events, friends, author, category, settings, blockContent, impressum]
 
+//about (List)
+// -> Subpage
+//Bibliothek....
+
+
+//Begriffe
+
+//Posttype -> Geschichte... ??
+
+//Content Type:
+// QFH - Heroes (Liste)
+// QFH - Geschichte
+
+// Begriffe
+
+// Coming Out
+
+// Beratungsstellen
+
+
+
+// Subsites
+// - Begriffe
+// - Beratungsstellen
+// - Unterstützer
+// -
+
+
+// Content Types
+// Artikel ( = Bibliothek)
+// Subsite
+// Heroes
+// Zeitstrahl
