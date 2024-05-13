d4ddab2405bf62f349f86f3202f69ac383b34363
Lucas Prabant <lprabant@gmail.com>
2024-05-13 16:53:13 +0200

    fixed a bug

--- app/(website)/infocorner/[slug]/default.js
+++ app/(website)/infocorner/[slug]/default.js
@@ -1,9 +1,15 @@
 import ThemeSiteWrapper from '@/components/themeSiteWrapper'
 import SubsitePage from '@/components/subsitePage'
+import Timeline from '@/components/timeline'
 
 export default function InfocornerPage({ infocornerPages, infocorner }) {
+  let siteContent = ''
 
-  const siteContent = <SubsitePage subsiteContent={infocorner} />
+ if (infocorner?._type === 'subsite') {
+    siteContent = <SubsitePage subsiteContent={infocorner} />
+  } else {
+    siteContent = <Timeline abschnitte={infocorner} />
+  }
 
   return (
     <ThemeSiteWrapper themenListe={infocornerPages.subsites} siteContent={siteContent} slug={infocorner.slug.current}
