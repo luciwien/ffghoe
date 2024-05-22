2b67a98537536e90c3c532ee6c017b27a144e19a
Lucas Prabant <lprabant@gmail.com>
2024-05-22 14:28:21 +0200

    kleine anpassungen.

--- app/(website)/queerfootballheroes/[slug]/default.js
+++ app/(website)/queerfootballheroes/[slug]/default.js
@@ -15,7 +15,7 @@ export default function QfhPage({ qfhPages, qfh, heroes }) {
       <p>Laut ÖFB gibt es ca. 600.000 Menschen, die in Österreich Fußball spielen. Es ist statistisch gesehen (in etwa
         10% der Menschen sind nicht heterosexuell) sehr unwahrscheinlich, dass keiner der männlichen Fußballer schwul
         ist – weder im Breitensport noch im Profibereich.</p>
-      <p>DDie meisten haben Angst, offen zu sich zu stehen. Wir wollen euch zeigen:<strong> Ihr seid gut, so wie ihr seid!</strong> Hier
+      <p>Die meisten haben Angst, offen zu sich zu stehen. Wir wollen euch zeigen:<strong> Ihr seid gut, so wie ihr seid!</strong> Hier
         ein paar prominente Beispiele offen queerer Fußball-Profis:</p>
       <div className='lg:grid lg:grid-cols-2 lg:gap-2'>
         {heroes.map((hero, i) => (<HeroesCard key={i} hero={hero} />))}
